a6bdd0b791b8ba1edb031ccc4405ead891791803
Bob Belderbos <bobbelderbos@gmail.com>
2021-09-16 08:10:00 +0200

    move code out of main

--- script.py
+++ script.py
@@ -1,3 +1,4 @@
+import argparse
 import os
 from time import sleep
 from urllib.parse import quote_plus
@@ -12,9 +13,7 @@ load_dotenv()
 CARBON_URL = "https://carbon.now.sh?l={language}&code={code}"
 CHROMEDRIVER_PATH = os.environ["CHROMEDRIVER_PATH"]
 # in case of a slow connection it might take a bit longer to download the image
-SECONDS_SLEEP_BEFORE_DOWNLOAD = int(
-    os.environ.get("SECONDS_SLEEP_BEFORE_DOWNLOAD", 3)
-)
+SECONDS_SLEEP_BEFORE_DOWNLOAD = int(os.environ.get("SECONDS_SLEEP_BEFORE_DOWNLOAD", 3))
 
 
 def create_code_image(code: str, language: str, headless: bool = True):
@@ -31,44 +30,48 @@ def create_code_image(code: str, language: str, headless: bool = True):
         sleep(SECONDS_SLEEP_BEFORE_DOWNLOAD)
 
 
-if __name__ == "__main__":
-    import argparse
+def get_args():
+    parser = argparse.ArgumentParser(description="Create a carbon code image")
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument("-f", "--file", type=str, help="File with code")
+    group.add_argument(
+        "-c", "--clipboard", action="store_true", help="Use code on clipboard"
+    )
+    group.add_argument("-s", "--snippet", type=str, help="Code snippet")
+    parser.add_argument(
+        "-l", "--language", type=str, default="python", help="Programming language"
+    )
+    parser.add_argument(
+        "-b",
+        "--browser",
+        action="store_true",
+        default=False,
+        help="Run Selenium in interactive (not headless) mode",
+    )
+    return parser.parse_args()
 
-    def get_args():
-        parser = argparse.ArgumentParser(description="Create a carbon code image")
-        group = parser.add_mutually_exclusive_group(required=True)
-        group.add_argument("-f", "--file", type=str, help="File with code")
-        group.add_argument(
-            "-c", "--clipboard", action="store_true", help="Use code on clipboard"
-        )
-        group.add_argument("-s", "--snippet", type=str, help="Code snippet")
-        parser.add_argument(
-            "-l", "--language", type=str, default="python", help="Programming language"
-        )
-        parser.add_argument(
-            "-b",
-            "--browser",
-            action="store_true",
-            default=False,
-            help="Run Selenium in interactive (not headless) mode",
-        )
-        return parser.parse_args()
 
-    def _get_code(args: argparse.Namespace) -> str:
-        """
-        Argparse's add_mutually_exclusive_group already guaranteers
-        we get one of clipboard, file or snippet
-        """
-        if args.clipboard:
-            code = pyperclip.paste()
-        elif args.file:
-            with open(args.file) as f:
-                code = f.read()
-        else:
-            code = args.snippet
+def get_code(args: argparse.Namespace) -> str:
+    """
+    Argparse's add_mutually_exclusive_group already guaranteers
+    we get one of clipboard, file or snippet
+    """
+    if args.clipboard:
+        code = pyperclip.paste()
+    elif args.file:
+        with open(args.file) as f:
+            code = f.read()
+    else:
+        code = args.snippet
 
-        return code
+    return code
 
+
+def main():
     args = get_args()
-    code = _get_code(args)
+    code = get_code(args)
     create_code_image(code, args.language, headless=not args.browser)
+
+
+if __name__ == "__main__":
+    main()
