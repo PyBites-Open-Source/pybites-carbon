c2b4c2e52513ef40be8f5d008deff153294ca7f4
Bob Belderbos <bobbelderbos@gmail.com>
2021-09-18 12:26:46 +0200

    enhance test to read text from image

--- tests/test_carbon.py
+++ tests/test_carbon.py
@@ -1,10 +1,12 @@
 from pathlib import Path
 
 import pytest
+import pytesseract
 
 from carbon.carbon import create_code_image
 
 CARBON_DOWNLOAD_FILE = Path("carbon.png")
+
 
 @pytest.fixture(autouse=True)
 def delete_previous_run():
@@ -15,3 +17,5 @@ def delete_previous_run():
 def test_create_standard_python_code_image():
     create_code_image("print('hello world')")
     assert CARBON_DOWNLOAD_FILE.exists()
+    image_text = pytesseract.image_to_string("carbon.png")
+    assert "hello world" in image_text
